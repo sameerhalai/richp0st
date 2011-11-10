e1f73ee03aded66342c241895246ea33308bc618
Sameer Halai <sameer@sameerhalai.com>
2011-11-09 23:49:36 -0800

    3

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ var giraffe = 0;
 
 http.createServer(function(request, response) {
 	response.writeHead(200, {"Content-Type": "text/plain"});
-	response.write("Hello World"+giraffe++);
+	response.write("Hello World3"+giraffe++);
 	response.end();
 }).listen(8888);
 
